fe7238f5c509391f1484c55407821ed0f4eef003
Adel Guitoun <adeelguitoun@gmail.com>
2022-10-25 21:42:49 +0100

    Revert "Math Magicians components"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import { Component } from 'react';
-import Calculator from './components/Calculator';
+import logo from './logo.svg';
+import './App.css';
 
-// APP
-/* eslint-disable */
-class App extends Component {
-  render() {
-    return <Calculator />;
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
