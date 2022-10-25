0c4412a8cea2d88cf2e37bb76f0b3bccdf2c661a
adelguit <adeelguitoun@gmail.com>
2022-10-25 20:44:28 +0100

    Implement class based components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import { Component } from 'react';
 import Calculator from './components/Calculator';
 
-const App = () => <Calculator />;
+// APP
+class App extends Component {
+  render() {
+    return <Calculator />;
+  }
+}
 
 export default App;
