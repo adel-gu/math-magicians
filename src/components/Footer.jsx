54d5c5d2419d15df1f090feaf018d915595a90ac
adelguit <adeelguitoun@gmail.com>
2022-11-04 15:18:00 +0100

    Fix eslint errors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,17 @@
 const Footer = () => {
   const mb = {
-    marginBottom: "10px"
-  }
+    marginBottom: '10px',
+  };
   return (
-    <footer className="footer" >
+    <footer className="footer">
       <div className="container header-footer py-2" style={mb}>
-        <span>Build by <a href="https://github.com/adel-gu">Adel Guitoun ❤️</a></span>
+        <span>
+          Build by
+          <a href="https://github.com/adel-gu">Adel Guitoun ❤️</a>
+        </span>
       </div>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
