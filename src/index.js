b8bab9de425b6719778bed380199e12f10ce26ee
adelguit <adeelguitoun@gmail.com>
2022-11-05 09:16:28 +0100

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { StrictMode } from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
@@ -8,9 +8,9 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>
+  </StrictMode>,
 );
